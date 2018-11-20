33be6c7ac637dff6e795dac1ab0027bb61272c7c
Marcin Radomski <marcin@mradomski.pl>
2018-11-20 22:07:13 +0100

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake
+
+
+class NestlConan(ConanFile):
+    name = "libnestl"
+    version = "0.1.0"
+    description = "Strictly noexcept container library"
+    topics = ("conan", "libnestl")
+    url = "https://github.com/dextero/nestl"
+    homepage = "https://github.com/dextero/nestl"
+    author = "Marcin Radomski <marcin@mradomski.pl>"
+    license = "MIT"
+    exports = ["LICENSE.md"]
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    no_copy_source = True
+    settings = "os", "arch", "compiler", "build_type"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        cmake.test()
+
+    def package(self):
+        self.copy("*.h")
+
+    def package_id(self):
+        self.info.header_only()

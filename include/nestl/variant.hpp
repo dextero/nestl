d87b11ab74db6bdaee924b6bec1e72c1e740d50a
Marcin Radomski <marcin@mradomski.pl>
2018-11-22 22:42:29 +0100

    variant improvements

--- include/nestl/variant.hpp
+++ include/nestl/variant.hpp
@@ -13,9 +13,18 @@ template <typename... Args>
 struct storage {
     alignas(max_alignment<Args...>) unsigned char data[max_sizeof<Args...>];
 
+    storage() = default;
+
+    storage(storage&&) = default;
+    storage& operator =(storage&&) = default;
+
+    storage(const storage&) = default;
+    storage& operator =(const storage&) = default;
+
     template <typename T, typename... CtorArgs>
-    void emplace(CtorArgs&&... args) {
+    storage& emplace(CtorArgs&&... args) {
         new (data) T(std::forward<CtorArgs>(args)...);
+        return *this;
     }
 
     template <typename T>
@@ -113,16 +122,54 @@ private:
         return { variant_type_error{} };
     }
 
+    template <typename T, typename... Args>
+    variant(tag<T>, Args&&... args)
+        : m_current(type_index<T, Ts...>),
+          m_storage(detail::storage<Ts...>{}.template emplace<T>(std::forward<Args>(args)...))
+    {
+        static_assert(is_one_of<T, Ts...>);
+    }
+
 public:
-    template <typename T>
+    template <
+        typename T,
+        typename = std::enable_if_t<
+            is_one_of<std::decay_t<T>, Ts...>
+        >
+    >
     variant(T&& t) {
         static_assert(is_one_of<std::decay_t<T>, Ts...>);
         m_current = construct<std::decay_t<T>, 0, Ts...>(std::forward<T>(t));
     }
 
+    variant(variant&& src) : m_current(invalid_type_index) {
+        *this = std::move(src);
+    }
+
+    variant& operator =(variant&& src) {
+        if (this != &src) {
+            this->~variant();
+
+            m_storage = std::move(src.m_storage);
+            std::swap(m_current, src.m_current);
+        }
+        return *this;
+    }
+
+    variant(const variant& src) = default;
+    variant& operator =(const variant& src) = default;
+
     ~variant() {
         destruct<0, Ts...>();
         m_current = invalid_type_index;
+    }
+
+    template <typename T, typename... Args>
+    static variant emplace(Args&&... args) {
+        return {
+            tag<T>{},
+            std::forward<Args>(args)...
+        };
     }
 
     template <typename T>

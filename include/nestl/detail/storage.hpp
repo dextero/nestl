ada84aea2c12c425afb8b6386abb7b80082eea21
Marcin Radomski <marcin@mradomski.pl>
2018-11-26 22:21:45 +0100

    Mark storage<> methods noexcept

--- include/nestl/detail/storage.hpp
+++ include/nestl/detail/storage.hpp
@@ -10,45 +10,45 @@ class storage {
 public:
     alignas(max_alignment<Args...>) unsigned char data[max_sizeof<Args...>];
 
-    storage() = default;
+    storage() noexcept = default;
 
-    storage(storage&&) = default;
-    storage& operator=(storage&&) = default;
+    storage(storage&&) noexcept = default;
+    storage& operator=(storage&&) noexcept = default;
 
     storage(const storage&) = delete;
     storage& operator=(const storage&) = delete;
 
     template <typename T, typename... CtorArgs>
-    storage(tag<T>, CtorArgs&&... args) {
+    storage(tag<T>, CtorArgs&&... args) noexcept {
         new (data) T(std::forward<CtorArgs>(args)...);
     }
 
     template <typename T>
-    T& as() & {
+    T& as() & noexcept {
         static_assert(is_one_of<T, Args...>);
         return *reinterpret_cast<T*>(data);
     }
 
     template <typename T>
-    T&& as() && {
+    T&& as() && noexcept {
         static_assert(is_one_of<T, Args...>);
         return std::move(*reinterpret_cast<T*>(data));
     }
 
     template <typename T>
-    const T& as() const {
+    const T& as() const noexcept {
         static_assert(is_one_of<std::remove_const_t<T>, Args...>);
         return *reinterpret_cast<const T*>(data);
     }
 
     template <typename T>
-    void destroy() {
+    void destroy() noexcept {
         destroy_impl<T, Args...>();
     }
 
 private:
     template <typename Query, typename T, typename... Ts>
-    void destroy_impl() {
+    void destroy_impl() noexcept {
         if (std::is_same_v<Query, T>) {
             as<T>().~T();
         } else {
@@ -57,7 +57,7 @@ private:
     }
 
     template <typename Query>
-    void destroy_impl() {}
+    void destroy_impl() noexcept {}
 };
 
 }  // namespace detail

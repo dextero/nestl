ba61f4f58ae35696de5eb7312697932a5cc93c32
Marcin Radomski <marcin@mradomski.pl>
2018-11-27 22:57:57 +0100

    Add copyright headers

--- include/nestl/detail/storage.hpp
+++ include/nestl/detail/storage.hpp
@@ -1,3 +1,9 @@
+//
+// Copyright (c) Marcin Radomski. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// details.
+//
 #pragma once
 
 #include <nestl/utility.hpp>
--- test/main.cpp
+++ test/main.cpp
@@ -1,2 +1,8 @@
+//
+// Copyright 2018 Marcin Radomski. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// details.
+//
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
